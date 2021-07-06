33bdd8c3c03ef7cfa01605e41d90ed4fdd0de231
Djoy <danielkjoy@gmail.com>
2021-07-06 12:00:59 -0400

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Daniel Joy on 7/6/21.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
